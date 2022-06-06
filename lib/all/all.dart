a7d9cb2a246877d65479d08519747a206bacedb5
SouhailaBenallal <souhaila.ben.allal@student.ehb.be>
2022-06-06 14:34:34 +0200

    Online or Offline Handyman

--- lib/all/all.dart
+++ lib/all/all.dart
@@ -1,9 +1,10 @@
+import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:geolocator/geolocator.dart';
 
 import '../models/handyman_model.dart';
 
 final FirebaseAuth fAuth = FirebaseAuth.instance;
 User? currentFribaseUser;
-
 ClientModel? clientModelCurrentinfo;
--- lib/tabPages/worklist_tab.dart
+++ lib/tabPages/worklist_tab.dart
@@ -1,7 +1,11 @@
 import 'dart:async';
-
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_application_reno/all/all.dart';
 import 'package:flutter_application_reno/assistants/assistant_methods.dart';
+import 'package:flutter_geofire/flutter_geofire.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -24,6 +28,11 @@ class _WorklistTabPageState extends State<WorklistTabPage> {
   Position? handymanCurrentPosition;
   var geoLocator = Geolocator();
   LocationPermission? _locationPermission;
+
+  String statusText = "Now Offline";
+  Color buttonColor = Colors.grey;
+  bool isHandymanActive = false;
+  StreamSubscription<Position>? streamSubscriptionPosition;
 
   blackThemeGoogleMap() {
     // Black theme
@@ -242,8 +251,127 @@ class _WorklistTabPageState extends State<WorklistTabPage> {
             blackThemeGoogleMap();
             locateHandymanPosition();
           },
+        ),
+
+        //ui for online offline handyman
+        statusText != "Now Online"
+            ? Container(
+                height: MediaQuery.of(context).size.height,
+                width: double.infinity,
+                color: Colors.black87,
+              )
+            : Container(),
+
+        //button for online offline handyman
+        Positioned(
+          top: statusText != "Now Online"
+              ? MediaQuery.of(context).size.height * 0.46
+              : 25,
+          left: 0,
+          right: 0,
+          child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            ElevatedButton(
+              onPressed: () {
+                // updateDriversLocationAtRealTime();
+                if (isHandymanActive != true) {
+                  updateHandymansLocationAtRealTime();
+                  handyIsOnlineNow();
+                  setState(() {
+                    statusText = "Now Online";
+                    isHandymanActive = true;
+                    buttonColor = Colors.transparent;
+                  });
+                  Fluttertoast.showToast(msg: "You are online now");
+                } else {
+                  handyIsOfflineNow();
+                  setState(() {
+                    statusText = "Now Offline";
+                    isHandymanActive = false;
+                    buttonColor = Colors.grey;
+                  });
+                  Fluttertoast.showToast(msg: "You are offline now");
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                  primary: buttonColor,
+                  padding: const EdgeInsets.symmetric(horizontal: 28),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(26))),
+              child: statusText != "Now Online"
+                  ? Text(
+                      statusText,
+                      style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    )
+                  : const Icon(
+                      Icons.phonelink_ring,
+                      color: Colors.white,
+                      size: 25,
+                    ),
+            )
+          ]),
         )
       ],
     );
+  }
+
+  handyIsOnlineNow() async {
+    Position pos = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+    handymanCurrentPosition = pos;
+    Geofire.initialize("activeHandymans");
+
+    Geofire.setLocation(currentFribaseUser!.uid,
+        handymanCurrentPosition!.latitude, handymanCurrentPosition!.longitude);
+    DatabaseReference ref = FirebaseDatabase.instance
+        .ref()
+        .child("handyman")
+        .child(currentFribaseUser!.uid)
+        .child("newLocationPlaceStatus");
+
+    ref.set("idle");
+    ref.onValue.listen((event) {});
+  }
+
+  updateHandymansLocationAtRealTime() {
+    streamSubscriptionPosition =
+        Geolocator.getPositionStream().listen((Position position) {
+      handymanCurrentPosition = position;
+
+      if (isHandymanActive == true) {
+        Geofire.setLocation(
+            currentFribaseUser!.uid,
+            handymanCurrentPosition!.latitude,
+            handymanCurrentPosition!.longitude);
+      }
+
+      LatLng latLng = LatLng(
+        handymanCurrentPosition!.latitude,
+        handymanCurrentPosition!.longitude,
+      );
+
+      newGoogleMapController!.animateCamera(CameraUpdate.newLatLng(latLng));
+    });
+  }
+
+  handyIsOfflineNow() {
+    Geofire.removeLocation(currentFribaseUser!.uid);
+
+    DatabaseReference? ref = FirebaseDatabase.instance
+        .ref()
+        .child("handyman")
+        .child(currentFribaseUser!.uid)
+        .child("newLocationPlaceStatus");
+    ref.onDisconnect();
+    ref.remove();
+    ref = null;
+
+    Future.delayed(const Duration(microseconds: 2000), () {
+      //SystemChannels.platform.invokeListMethod("SystemNavigator.pop");
+      SystemNavigator.pop();
+    });
   }
 }
